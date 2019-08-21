5aefcf0a98ba96ba37da8edc91dede0050c4c1f7
jzt <jztangw@gmail.com>
2019-08-21 12:16:53 -0700

    add folder function to let user designate which folder to put in

--- gen.sh
+++ gen.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 read -p "Enter the title: " title 
+read -p "Enter which folder you want to put in (relative to '_post/'): " fld
+# delete trailing slash if there is any
+Folder=`echo $fld | sed -e 's#/$##'`
 Title=`echo $title | tr -d '[:punct:]'`
 dashedTitle=""
 for word in $Title
@@ -27,5 +30,11 @@ echo "updated:" >> $filename
 echo "toc: true" >> $filename
 echo "---" >> $filename
 echo "" >> $filename
+if [ -z "${Folder}" ]
+then
 mv $filename _posts/$filename
 vim _posts/$filename
+else
+mv $filename _posts/$Folder/$filename
+vim _posts/$Folder/$filename
+fi
